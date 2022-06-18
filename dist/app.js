9888d1396a19a9a9e60678e1b408332005922899
duyanh14 <duyanhnguyen2001@gmail.com>
2022-06-18 21:59:46 +0700

    Build

--- dist/app.js
+++ dist/app.js
@@ -6,6 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const userRoutes = require('./routers/userRoutes');
 const app = (0, express_1.default)();
+app.get('/', function (req, res, next) {
+    res.status(200).send('Spellcasters');
+});
 app.use('/api/user', userRoutes);
 app.use((req, res, next) => {
     return res.status(404).send();
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,10 @@ import express from 'express'
 const userRoutes = require('./routers/userRoutes');
 const app = express();
 
+app.get('/', function(req, res, next) {
+    res.status(200).send('Spellcasters');
+});
+
 app.use('/api/user', userRoutes);
 
 app.use((req, res, next) => {
