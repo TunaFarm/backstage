0299989b822d8add9692d5fd9a46bb0361a7764d
Hoang Tien Minh <htminh.1001@gmail.com>
2022-06-26 11:14:02 +0700

    feat: add endpoint to generate roomId or return existed roomId

--- src/routers/ws/playRoutes.ts
+++ src/routers/ws/playRoutes.ts
@@ -1,17 +1,37 @@
 /* eslint-disable no-console */
-import { Request, Router } from 'express';
+import { Request, Response, Router } from 'express';
 import expressWs from 'express-ws';
 import { WebSocket } from 'ws';
+import { v4 as uuid } from 'uuid';
 
 // TODO: Implement room logic later
-// const roomList = new Map<string, Map<string, WebSocket>>();
+const roomList = new Map<string, Map<string, WebSocket>>();
 
-// TODO: Need a middleware to check all ws info before connection to ws
 const getPlayRoutes = (): expressWs.Router => {
   const playRoute = Router();
 
+  // Generate new roomId or return existed roomId
+  playRoute.post('/', (req: Request, res: Response) => {
+    let id;
+    roomList.forEach((roomInfo, roomId) => {
+      if (roomInfo.size < 4) {
+        id = roomId;
+      }
+    });
+
+    if (!id) {
+      id = uuid();
+      roomList.set(id, new Map<string, WebSocket>());
+    }
+
+    res.status(200).send({ id });
+  });
+
+  // Setup ws events when connected
   playRoute.ws('/:id', (ws: WebSocket, req: Request) => {
     const roomId = req.params.id;
+
+    console.log(req.headers);
 
     if (!roomId) {
       console.log('Ws Connection Failed: Room Id not provided');
